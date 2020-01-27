89f25846bab7ac046487c5ac5505bc1bad154833
Ricardo Virtudazo <me@ricardovirtudazo.com>
2020-01-27 18:00:37 +0100

    Remove try-catch in Speaker.Register (this is handled in .TryRegister.

--- CodeLuau/Speaker.cs
+++ CodeLuau/Speaker.cs
@@ -24,10 +24,10 @@ namespace CodeLuau
         public List<string> Certifications { get; set; }
         public string BlogURL { get; set; }
         public string Employer { get; set; }
-
         public WebBrowser Browser { get; set; }
 		public int RegistrationFee { get; set; }
 		public List<ConferenceSession> ProposedConferenceSessions { get; set; }
+
 
 		public RegisterResponse TryRegister(IRepository repository)
 		{
@@ -48,37 +48,22 @@ namespace CodeLuau
             if (registerError != null) 
                 return new RegisterResponse(registerError);
 
-            int? speakerId = null;
             var valuedExp = ExperienceYearCount ?? 0;
-            RegistrationFee
-                = RegistrationFeeDefaults.VariableFeeList
-                    .First(fee => fee.IsQualifiedExperienceYears(valuedExp))
-                    .Amount;
+            RegistrationFee = RegistrationFeeDefaults.VariableFeeList
+                .First(fee => fee.IsQualifiedExperienceYears(valuedExp))
+                .Amount;
 
-            try
-            {
-                speakerId = repository.SaveSpeaker(this);
-            }
-            catch (Exception e)
-            {
-                //in case the db call fails 
-            }
-
-            return new RegisterResponse((int)speakerId);
+            var speakerId = repository.SaveSpeaker(this);
+            return new RegisterResponse(speakerId);
 		}
 
         private RegisterError? GetRegisterError()
         {
-            if (IsFirstNameEmpty)
-                return RegisterError.FirstNameRequired;
-
-            if (IsLastNameEmpty)
-                return RegisterError.LastNameRequired;
+            if (IsFirstNameEmpty) return RegisterError.FirstNameRequired;
+            if (IsLastNameEmpty) return RegisterError.LastNameRequired;
+            if (email.IsEmpty) return RegisterError.EmailRequired;
             
-            if (email.IsEmpty)
-                return RegisterError.EmailRequired;
-            
-            if (!MeetsStandards())
+            if (!MeetsStandards()) 
                 return RegisterError.SpeakerDoesNotMeetStandards;
             
             if (!ProposedConferenceSessions.Any())
